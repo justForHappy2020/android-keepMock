b53762ba1a1075aebcaa2036acc870d676f8f2a7
siting lin <1446732363@qq.com>
2020-11-17 21:08:02 +0800

    lst

--- app/src/main/java/com/example/myapplication/exercise_main.java
+++ app/src/main/java/com/example/myapplication/exercise_main.java
@@ -10,5 +10,6 @@ public class exercise_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_main);
+
     }
 }

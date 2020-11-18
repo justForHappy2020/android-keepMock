5afc07af3a32c8d2fa38544804748f6002cdf62e
devin413 <devin413@163.com>
2020-11-18 16:49:07 +0800

    11.18devin+写了部分课程主页

--- app/src/main/java/com/example/myapplication/exercise_main.java
+++ app/src/main/java/com/example/myapplication/exercise_main.java
@@ -13,13 +13,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.myapplication.utils.HttpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class exercise_main extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton ibSearch;
     private Button btBodypart[] = new Button[8];
-    private Button btHotcourse[] = new Button[9];
-
+    private Button btHotcourse[] = new Button[10];
+    private TextView tvCountTime;
+    private List<String> bodyPart;
+    private List<String> degree;
+    private List hotCourse = new ArrayList();
 
     private int httpcode;
 
@@ -29,9 +42,86 @@ public class exercise_main extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_exercise_main);
 
         initView();
+        initCountTime();
+        initBodypart();
+        initHotCourse();
+    }
+
+    private void initHotCourse(){
+        String url = "http://192.168.16.1:8080/api/course/getHotCourse10";
+        String responseData = null;
+        try {
+            responseData = HttpUtils.connectHttpGet(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject1 = null;
+        try {
+            jsonObject1 = new JSONObject(responseData);
+            httpcode = jsonObject1.getInt("code");
+            if(httpcode == 200){
+                JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
+                //待写：得到hotCourse的list
+
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 10; i++)btHotcourse[i].setText(hotCourse.indexOf(1));//待写：得到热门课程的名称
+
+    }
+
+    private void initBodypart(){
+        String url = "http://192.168.16.1:8080/api/course/getFilter";
+        String responseData = null;
+        try {
+            responseData = HttpUtils.connectHttpGet(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject1 = null;
+        try {
+            jsonObject1 = new JSONObject(responseData);
+            httpcode = jsonObject1.getInt("code");
+            if(httpcode == 200){
+                JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
+                //bodyPart = jsonObject2.getJSONArray("bodyPart");
+                //待写：得到bodypart、degree 的list
+
+
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 8; i++)btBodypart[i].setText(bodyPart.get(0));
+    }
+
+    private void initCountTime()  {
+        String countTime = "0";
+        String url = "http://192.168.16.1:8080/api/course/getCountTime";
+        String responseData = null;
+        try {
+            responseData = HttpUtils.connectHttpGet(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject1 = null;
+        try {
+            jsonObject1 = new JSONObject(responseData);
+            httpcode = jsonObject1.getInt("code");
+            if(httpcode == 200)countTime = String.valueOf(jsonObject1.getInt("data"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        tvCountTime.setText("已累计运动" + countTime + "分钟");
     }
 
     private void initView() {
+        tvCountTime = findViewById(R.id.accumulate_time);
         ibSearch = findViewById(R.id.search);
         btBodypart[0] = findViewById(R.id.bodypart1);
         btBodypart[1] = findViewById(R.id.bodypart2);
@@ -51,11 +141,14 @@ public class exercise_main extends AppCompatActivity implements View.OnClickList
         btHotcourse[6] = findViewById(R.id.hotcourse7);
         btHotcourse[7] = findViewById(R.id.hotcourse8);
         btHotcourse[8] = findViewById(R.id.hotcourse9);
+        btHotcourse[9] = findViewById(R.id.hotcourse10);
+
+
 
         ibSearch.setOnClickListener(this);//监听获取验证码按钮
         int i;
         for (i = 0; i < 8; i++) btBodypart[i].setOnClickListener(this);
-        for (i = 0; i < 9; i++) btBodypart[i].setOnClickListener(this);
+        for (i = 0; i < 10; i++) btHotcourse[i].setOnClickListener(this);
 
 
     }
@@ -98,6 +191,8 @@ public class exercise_main extends AppCompatActivity implements View.OnClickList
             case R.id.hotcourse8:
                 break;
             case R.id.hotcourse9:
+                break;
+            case R.id.hotcourse10:
                 break;
         }
     }
--- app/src/main/java/com/example/myapplication/utils/HttpUtils.java
+++ app/src/main/java/com/example/myapplication/utils/HttpUtils.java
@@ -17,20 +17,15 @@ public class HttpUtils {
         //设置JSON数据
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         //okhttp请求
-        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(60000, TimeUnit.MILLISECONDS)
+        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(30000, TimeUnit.MILLISECONDS)
                 .readTimeout(30000, TimeUnit.MILLISECONDS)
                 .build();
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //OkHttpClient.Builder builder = new OkHttpClient.Builder();
         RequestBody requestBody = RequestBody.create(JSON, String.valueOf(json));
         Request request = new Request.Builder()
                 .url(url)
                 .post(requestBody)
                 .build();
-
-        System.out.println("requestBody");
-        System.out.println(requestBody);
-        System.out.println("request");
-        System.out.println(request);
 
         Response response = client.newCall(request).execute();
         if (!response.isSuccessful())
@@ -39,8 +34,18 @@ public class HttpUtils {
         return  responseData;
     }
 
-/*    static public String connectHttpGet(String url){
-        String responseData;
+    static public String connectHttpGet(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(30000, TimeUnit.MILLISECONDS)
+                .readTimeout(30000, TimeUnit.MILLISECONDS)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful())
+        {throw new IOException("Unexpected code" + response);}
+        String responseData = response.body().string();
         return  responseData;
-    }*/
+    }
 }
